10809010dbe985f9f50708b800dbadcb03485ac1
guchi0 <sp9g83t7@gmail.com>
2021-03-20 14:24:19 +0900

    musichouseの追加

---
+++ music house/js/dustbox.js
@@ -0,0 +1,17 @@
+
+
+function drum(a) {
+    if (a == 0) {
+        var audio = new Audio("./sound/sound_dust/bassdrum.wav");
+    } else if (a == 1) {
+        var audio = new Audio("./sound/sound_dust/cymbal.wav");
+    } else if (a == 2) {
+        var audio = new Audio("./sound/sound_dust/hat.wav");
+    } else if (a == 3) {
+        var audio = new Audio("./sound/sound_dust/snare.wav");
+    } else if (a == 4) {
+        var audio = new Audio("./sound/sound_dust/tom.wav");
+    }
+
+    audio.play();
+}
